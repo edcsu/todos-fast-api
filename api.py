9e61ca97aedd58b8c9af322aebefc436df1df2da
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-01-25 00:00:09 +0300

    feat: add todos routes

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from todo import todo_router
 
 app = FastAPI()
 
 @app.get("/")
 async def welcom() -> dict:
 	return {"message": "Hello World"}
+
+app.include_router(todo_router)
+
---
+++ todo.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+todo_router = APIRouter()
+
+todo_list = []
+
+@todo_router.post("/todos")
+async def add_todo(todo: dict) -> dict:
+	todo_list.append(todo)
+	return {"message": "Todo succesfully added"}
+
+@todo_router.get("/todos")
+async def retrieve_todos() -> dict:
+	return {"todos": todo_list}
+
+
