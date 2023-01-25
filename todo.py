a197ed4171e07681c86b9711d46a4996d64ccb35
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-01-25 18:16:20 +0300

    feat: add get single todo endpoint

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from model import Todo
 
 todo_router = APIRouter()
@@ -8,10 +8,23 @@ todo_list = []
 @todo_router.post("/todos")
 async def add_todo(todo: Todo) -> dict:
 	todo_list.append(todo)
-	return {"message": "Todo succesfully added"}
+	return {
+		"message": "Todo succesfully added"
+	}
 
 @todo_router.get("/todos")
 async def retrieve_todos() -> dict:
-	return {"todos": todo_list}
+	return {
+		"todos": todo_list
+	}
 
-
+@todo_router.get("/todos/{todo_id}")
+async def get_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {
+                "todo": todo
+			}
+    return {
+			"message": "Todo with supplied ID doesn't exist."
+		}
