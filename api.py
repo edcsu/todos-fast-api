28711121139e41fc335b141bcc2df7982d0b0bf2
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-01-25 18:43:57 +0300

    style: enrich open API schema

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from todo import todo_router
 app = FastAPI()
 
 @app.get("/")
-async def welcom() -> dict:
+async def welcome() -> dict:
 	return {"message": "Hello World"}
 
 app.include_router(todo_router)
