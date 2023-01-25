38fddccfe70fd0df71f8fb2989a06510ed80f1d6
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-01-25 18:57:11 +0300

    feat: add update endpoint

--- model.py
+++ model.py
@@ -11,3 +11,12 @@ class Todo(BaseModel):
                 "item": "Example schema!"
             }
         }
+
+class TodoItem(BaseModel):
+   item: str
+   class Config:
+       schema_extra = {
+           "example": {
+               "item": "Read the next chapter of the book"
+            } 
+        }
--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Path
-from model import Todo
+from model import Todo, TodoItem
 
 todo_router = APIRouter()
 
@@ -28,3 +28,15 @@ async def get_todo(todo_id: int = Path(..., title="The ID of the todo to retriev
     return {
 			"message": "Todo with supplied ID doesn't exist."
 		}
+
+@todo_router.put("/todos/{todo_id}")
+async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The ID of the todo to be updated")) -> dict:
+   for todo in todo_list:
+       if todo.id == todo_id:
+           todo.item = todo_data.item
+           return {
+               "message": "Todo updated successfully."
+           }
+   return {
+       "message": "Todo with supplied ID doesn't exist."
+	}
